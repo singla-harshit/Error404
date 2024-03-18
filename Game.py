dbd12ccf43e0a276c948d8ffbf50da9b162995ff
singla-harshit <harshitsinglaofficial23@gmail.com>
2024-03-18 21:41:45 +0530

    Added Play Again and rules for the game

--- Game.py
+++ Game.py
@@ -1,22 +1,17 @@
-<<<<<<< HEAD
 import tkinter as tk
 from tkinter import messagebox
 import random
 
+# Hangman graphics
 Hangman_arr = ['''
   +---+
-       |
-       |
-       |
+      |
+      |
+      |
       ===''', '''
   +---+
   O   |
       |
-      |
-     ===''', '''
-  +---+
-  O   |
-  |   |
       |
      ===''', '''
   +---+
@@ -26,214 +21,139 @@ Hangman_arr = ['''
      ===''', '''
   +---+
   O   |
- /|\  |
+ /|\\  |
       |
      ===''', '''
   +---+
   O   |
- /|\  |
+ /|\\  |
  /    |
-      |
      ===''', '''
   +---+
   O   |
- /|\  |
- / \  |
-      |
+ /|\\  |
+ / \\  |
      ===''']
 
+# List of words for the game
 mainwords = 'sleeve fancade stress reform working busy writer forget beleive summer chance rabbit install damage error developer coder flight broken prince bother translater language mind life better india account monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork religion man women children master piece movie film photo'.split()
 
 
-def randomwords(word):
-    wordindex = random.randint(0, int(len(mainwords) - 1))
-    return word[wordindex]
+def random_word(word_list):
+    return random.choice(word_list)
 
 
-def showfunc(wrong, correct, randomword):
-    display_text = Hangman_arr[len(wrong) - 1]
-    display_text += '\n\nWrong words entered by user: ' + ', '.join(wrong)
-    display_text += '\nCorrect words: '
-    blanks = '_' * len(randomword)
-    for i in range(len(randomword)):
-        if randomword[i] in correct:
-            blanks = blanks[:i] + randomword[i] + blanks[i + 1:]
-    display_text += ' '.join(list(blanks))
-    return display_text
+class HangmanGame:
+    def _init_(self, master):
+        self.master = master
+        self.master.title("Hangman Game")
 
+        # Rules button
+        self.rules_button = tk.Button(master, text="Rules", command=self.display_rules, font=('Arial', 16))
+        self.rules_button.pack()
 
-def guessfunc(already):
-    while True:
-        guess = input('Guess a letter: ').lower()
-        if len(guess) != 1:
-            print('Enter a single letter.')
-        elif guess in already:
-            print('The letter is already guessed.')
-        elif guess not in 'abcdefghijklmnopqrstuvwxyz':
-            print("Enter a valid alphabet.")
-        else:
-            return guess
+        self.word = random_word(mainwords)
+        self.remaining_guesses = len(Hangman_arr)
+        self.correct_guesses = []
+        self.incorrect_guesses = []
 
+        # Hangman display
+        self.hangman_display = tk.Label(master, text="", font=('Arial', 14))
+        self.hangman_display.pack()
 
-def play_again():
-    global wrong, correct, randomword
-    wrong = ''
-    correct = ''
-    randomword = randomwords(mainwords)
-    hangman_display.config(text=showfunc(wrong, correct, randomword))
-    play_again_button.config(state=tk.DISABLED)
+        # Correct and incorrect guesses
+        self.correct_guesses_label = tk.Label(master, text="Correct guesses: ", font=('Arial', 14))
+        self.correct_guesses_label.pack()
 
+        self.incorrect_guesses_label = tk.Label(master, text="Incorrect guesses: ", font=('Arial', 14))
+        self.incorrect_guesses_label.pack()
 
-def check_guess():
-    global wrong, correct
-    guess = guess_entry.get().lower()
-    if len(guess) != 1 or not guess.isalpha():
-        messagebox.showerror("Error", "Please enter a single alphabetical character.")
-        return
+        # Guess entry
+        self.guess_entry = tk.Entry(master, font=('Arial', 16))
+        self.guess_entry.pack()
 
-    if guess in wrong or guess in correct:
-        messagebox.showinfo("Info", f"You already guessed '{guess}'.")
-        return
+        # Guess button
+        self.guess_button = tk.Button(master, text="Guess", command=self.check_guess, font=('Arial', 16))
+        self.guess_button.pack()
 
-    if guess in randomword:
-        correct += guess
-    else:
-        wrong += guess
-        hangman_display.config(text=showfunc(wrong, correct, randomword))
-        if len(wrong) == len(Hangman_arr) - 1:
-            messagebox.showinfo("Game Over", "Sorry, you lost.")
-            play_again_button.config(state=tk.NORMAL)
+        # End Game button
+        self.end_game_button = tk.Button(master, text="End Game", command=self.end_game, font=('Arial', 16))
+        self.end_game_button.pack()
+
+        # Play Again button
+        self.play_again_button = tk.Button(master, text="Play Again", command=self.play_again, font=('Arial', 16),
+                                            state=tk.NORMAL)
+        self.play_again_button.pack()
+
+        self.update_hangman_display()
+
+    def display_rules(self):
+        messagebox.showinfo("Rules", "The rules of Hangman are simple:\n"
+                                     "1. Try to guess the word by entering one letter at a time.\n"
+                                     "2. You have a limited number of guesses.\n"
+                                     "3. Every incorrect guess adds a part to the hangman.\n"
+                                     "4. Guess all the letters before the hangman is complete to win the game.")
+
+    def update_hangman_display(self):
+        hangman_index = len(Hangman_arr) - self.remaining_guesses
+        self.hangman_display.config(text=Hangman_arr[hangman_index])
+        if hangman_index == 5:
+            
+            messagebox.showinfo("Game Over", " you lost the game.")
+            hangman_index = 5
+            self.word = random_word(mainwords)
+            self.remaining_guesses = len(Hangman_arr)
+            self.correct_guesses = []
+            self.incorrect_guesses = []
+            self.correct_guesses_label.config(text="Correct guesses: ")
+            self.incorrect_guesses_label.config(text="Incorrect guesses: ")
+            self.play_again_button.config(state=tk.NORMAL)
+            self.update_hangman_display()
             return
 
-    if set(randomword) <= set(correct):
-        messagebox.showinfo("Congratulations", "Congratulations, you won!")
-        play_again_button.config(state=tk.NORMAL)
+    def play_again(self):
+        self.word = random_word(mainwords)
+        self.remaining_guesses = len(Hangman_arr)
+        self.correct_guesses = []
+        self.incorrect_guesses = []
+        self.correct_guesses_label.config(text="Correct guesses: ")
+        self.incorrect_guesses_label.config(text="Incorrect guesses: ")
+        self.play_again_button.config(state=tk.NORMAL)
+        self.update_hangman_display()
+
+    def check_guess(self):
+        guess = self.guess_entry.get().lower()
+        self.guess_entry.delete(0, tk.END)  # Clear the entry
+
+        if not guess.isalpha() or len(guess) != 1:
+            messagebox.showerror("Error", "Please enter a single alphabetical character.")
+            return
+
+        if guess in self.correct_guesses or guess in self.incorrect_guesses:
+            messagebox.showinfo("Info", f"You already guessed '{guess}'.")
+            return
+
+        if guess in self.word:
+            self.correct_guesses.append(guess)
+            self.correct_guesses_label.config(text=f"Correct guesses: {', '.join(self.correct_guesses)}")
+            if set(self.word) <= set(self.correct_guesses):
+                messagebox.showinfo("Congratulations", "Congratulations, you won!")
+                self.play_again_button.config(state=tk.NORMAL)
+        else:
+            self.incorrect_guesses.append(guess)
+            self.incorrect_guesses_label.config(text=f"Incorrect guesses: {', '.join(self.incorrect_guesses)}")
+            self.remaining_guesses -= 1
+            self.update_hangman_display()
+
+    def end_game(self):
+        self.master.destroy()
 
 
-root = tk.Tk()
-root.title("Hangman Game")
-
-wrong = ''
-correct = ''
-randomword = randomwords(mainwords)
-
-hangman_display = tk.Label(root, text=showfunc(wrong, correct, randomword), font=('Arial', 14))
-hangman_display.pack()
-
-guess_entry = tk.Entry(root, font=('Arial', 16))
-guess_entry.pack()
-
-guess_button = tk.Button(root, text="Guess", command=check_guess, font=('Arial', 16))
-guess_button.pack()
-
-play_again_button = tk.Button(root, text="Play Again", command=play_again, font=('Arial', 16), state=tk.DISABLED)
-play_again_button.pack()
-
-root.mainloop()
-=======
-import random
-Hangman_arr=['''
-  +---+
-       |
-       |
-       |
-      ===''', '''
-  +---+
-  O   |
-      |
-      |
-     ===''', '''
-  +---+
-  O   |
-  |   |
-      |
-     ===''', '''
-  +---+
-  O   |
- /|   |
-      |
-     ===''', '''
-  +---+
-  O   |
- /|\  |
-      |
-     ===''', '''
-  +---+
-  O   |
- /|\  |
- /    |
-      |
-     ===''', '''
-  +---+
-  O   |
- /|\  |
- / \  |
-      |
-     ===''']
-mainwords = 'sleeve fancade stress reform working busy writer forget beleive summer chance rabbit install damage error developer coder flight broken prince bother translater language mind life better india account monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork religion man women children master piece movie film photo'.split()
-def randomwords(word):
-  wordindex=random.randint(0,int(len(mainwords)-1))
-  return word[wordindex]
-def showfunc(wrong,correct,randomword):
-  print(Hangman_arr[len(wrong)-1])
-  print()
-  print('Wrong words enter by user : ',end=' ')
-  for letters in wrong:
-    print(letters,end=' ')
-  print() 
-  print("Correct words : ",end="")
-  blanks= '_' * len(randomword)
-  for i in range(len(randomword)):
-    if randomword[i] in correct:
-      blanks=blanks[:i]+randomword[i]+blanks[i+1:]
-  for letter in blanks:
-    print(letter,end='')
-  print()  
-def guessfunc(already):
-  while True:
-    print('guess a letter')
-    guess=input()
-    guess=guess.lower()
-    if len(guess)!=1:
-      print('enter the single letter')
-    elif guess in already:
-      print('enter alphabet is  already guessed')
-    elif guess not in 'abcdefghijklmnopqrstuvwxyz':
-      print("enter valid alphabet")
-    else:
-      return guess
-
-print("Welcome!!")      
-wrong=''
-correct=''
-randomword=randomwords(mainwords)
-
-endgame=False
-while True:
-
-  showfunc(wrong,correct,randomword)
-  guess1=guessfunc(wrong+correct)
-  if guess1 in randomword:
-    correct=correct+guess1
-    foundall=True
-    for i in range(len(randomword)):
-      if(randomword[i] not in correct):
-        foundall=False
-        break
-    if foundall:
-      print("You win")
-      endgame=True
-      print("Please give the feedback to the game")
-      feedback=input()
-      print("Thanks for the feedback")
-      break
-  else:
-    wrong=wrong+guess1
-    if len(wrong)==len(Hangman_arr)-1:
+def main():
+    root = tk.Tk()
+    hangman = HangmanGame(root)
+    root.mainloop()
 
 
-      showfunc(wrong,correct,randomword)
-      print("Correct guesses is "+str(len(correct)) + "and wrong guesses is"+ str(len(wrong)))
-      print("Try again chances are over")
-      break
->>>>>>> 670d193b5fbf12e09d6749e8c3d752cef7f621a8
+if __name__ == "_main_":
+    main()
